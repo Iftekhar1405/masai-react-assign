bbc903ac7a93baaf5065847b0268ce6d38ca8323
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-09-18 12:36:12 +0530

    problem -02 solved

---
+++ day-09/2/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useReducer } from 'react'
+
+function App() {
+  const initial = 0
+  const reducer = (state,action) => {
+    switch (action.type){
+      case 'inc':
+        return state + 1
+      case 'dec':
+        return state - 1
+    }
+  }
+  const [count, dispatch] = useReducer(reducer,initial)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => dispatch({type :'inc'})}>
+          count is {count}
+        </button>
+        <button onClick={() => dispatch({type :'dec'})}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
